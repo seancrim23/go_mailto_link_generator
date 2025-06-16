4843216927e6fbca0e427267fc34da8f869793e4
sean <sean.g.crim@gmail.com>
2025-06-16 16:42:47 -0400

    built out handlers and business logic for services should be ready to go

--- handler/mail.go
+++ handler/mail.go
@@ -1,8 +1,14 @@
 package handler
 
 import (
+	"errors"
+	"fmt"
+	"html/template"
 	"mailto_link_generator/services"
+	"mailto_link_generator/utils"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 type Mail struct {
@@ -10,13 +16,67 @@ type Mail struct {
 }
 
 func (m *Mail) GetMailToForm(w http.ResponseWriter, r *http.Request) {
-	//TODO build this out
+	//TODO update to pull template from cloud file storage
+	tmpl := template.Must(template.ParseFiles("C:/development/go_mailto_link_generator/templates/mailto.html"))
+	fmt.Println("getting the mailto for the home page...")
+	tmpl.Execute(w, nil)
 }
 
 func (m *Mail) CreateMailtoLink(w http.ResponseWriter, r *http.Request) {
-	//TODO build this out
+	defer r.Body.Close()
+	var err error
+	var to string
+	var subject string
+	var body string
+
+	tmpl := template.Must(template.ParseFiles("C:/development/go_url_shortener_practice/templates/mailto.html"))
+	fmt.Println("getting the mailto for the home page...")
+
+	fmt.Println("creating short url...")
+	to = r.FormValue("to")
+	subject = r.FormValue("subject")
+	body = r.FormValue("body")
+
+	if to == "" || subject == "" || body == "" {
+		tmpl.Execute(w, map[string]string{"Error": "invalid form entries", "Code": "400"})
+	}
+
+	mailToLink, err := m.Service.CreateMailtoLink(r.Context(), to, subject, body)
+	//determine what type of error and change code and return according error message
+	if err != nil {
+		tmpl.Execute(w, map[string]string{"Error": err.Error(), "Code": "500"})
+	}
+
+	tmpl.Execute(w, map[string]string{"MailToURL": mailToLink, "Generated": "true"})
 }
 
 func (m *Mail) GetMailtoLink(w http.ResponseWriter, r *http.Request) {
-	//TODO build this out
+	var code = 200
+	var err error
+
+	shortLink := chi.URLParam(r, "id")
+	if shortLink == "" {
+		code = 400
+		utils.RespondWithError(w, code, errors.New("no shortLink passed to request").Error())
+		return
+	}
+
+	//TODO get a check somewhere in here eventually to see if the url is in the cache
+
+	mailToLink, err := m.Service.GetMailtoLink(r.Context(), shortLink)
+	//determine what type of error and change code and return according error message
+	if err != nil {
+		code = 500
+		utils.RespondWithError(w, code, err.Error())
+		return
+	}
+	if mailToLink == "" {
+		code = 404
+		utils.RespondWithError(w, code, errors.New("cannot find mailto link").Error())
+		return
+	}
+
+	http.Redirect(w, r, mailToLink, 302)
+	//should the backend redirect or should the front end do the redirect?
+	utils.RespondWithJSON(w, code, mailToLink)
 }
--- services/firestore_mailto_generator_service.go
+++ services/firestore_mailto_generator_service.go
@@ -2,6 +2,11 @@ package services
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"mailto_link_generator/models"
+	"math/rand"
+	"time"
 
 	"cloud.google.com/go/firestore"
 )
@@ -11,9 +16,61 @@ type FirestoreMailtoGeneratorService struct {
 }
 
 func (f *FirestoreMailtoGeneratorService) CreateMailtoLink(ctx context.Context, to string, subject string, body string) (string, error) {
-	return "finish this function", nil
+	shortUrl := generateShortUrl(8)
+
+	builtUrl := &models.Mail{
+		Id:      shortUrl,
+		To:      to,
+		Subject: subject,
+		Body:    body,
+	}
+
+	_, err := f.Database.Collection("URL").Doc(shortUrl).Set(ctx, builtUrl)
+	if err != nil {
+		fmt.Println("error generating short url")
+		fmt.Println(err)
+		return "", errors.New("error generating short url")
+	}
+
+	return shortUrl, nil
+}
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+// basically from the internet
+// TODO update if theres any custom url or more fancy url generation i find
+func generateShortUrl(length int) string {
+	seededRand := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	var result []byte
+
+	for i := 0; i < length; i++ {
+		index := seededRand.Intn(len(charset))
+		result = append(result, charset[index])
+	}
+
+	return string(result)
 }
 
 func (f *FirestoreMailtoGeneratorService) GetMailtoLink(ctx context.Context, id string) (string, error) {
-	return "finish this function", nil
+	dsnap, err := f.Database.Collection("URL").Doc(id).Get(ctx)
+	if err != nil {
+		fmt.Println("error getting long url")
+		fmt.Println(err)
+		return "", errors.New("error getting long url")
+	}
+
+	var m models.Mail
+	err = dsnap.DataTo(&m)
+	if err != nil {
+		fmt.Println(err)
+		return "", errors.New("error getting long url")
+	}
+
+	mailToLink := generateMailToLink(m.To, m.Subject, m.Body)
+	return mailToLink, nil
+}
+
+func generateMailToLink(to string, subject string, body string) string {
+	return fmt.Sprintf("mailto:%s?subject=%s&body=%s", to, subject, body)
 }
